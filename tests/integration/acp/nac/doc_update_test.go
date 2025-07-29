fea612fb5d0bcf936ee7315900bf0eff64168fc9
Shahzad Lone <shahzadlone@gmail.com>
2025-07-29 14:05:37 -0400

    PR(MAIN): Gate & Test - Collection Doc Operations

---
+++ tests/integration/acp/nac/doc_update_test.go
@@ -0,0 +1,179 @@
+// Copyright 2025 Democratized Data Foundation
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
+package test_acp_nac
+
+import (
+	"testing"
+
+	testUtils "github.com/sourcenetwork/defradb/tests/integration"
+)
+
+func TestNAC_GatesDocUpdate_AllowIfAuthorizedElseError(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "admin acp correctly gates doc update operation, allow if authorized, otherwise error",
+		Actions: []any{
+			// Starting with NAC, so only authorized user(s) can perform operations from here on out.
+			testUtils.Close{},
+			testUtils.Start{
+				Identity:  testUtils.ClientIdentity(1),
+				EnableNAC: true,
+			},
+			// Note: Doing setup steps after starting with nac enabled, otherwise the in-memory tests
+			// will loose setup state when the restart happens (i.e. the restart that started nac).
+			testUtils.SchemaUpdate{
+				Identity: testUtils.ClientIdentity(1),
+				Schema: `
+					type User {
+						name: String
+						age: Int 
+					}`,
+			},
+			testUtils.CreateDoc{
+				Identity:     testUtils.ClientIdentity(1),
+				CollectionID: 0,
+				Doc: `{
+					"name": "Shahzad",
+					"age": 48
+				}`,
+			},
+
+			// We haven't authorized non-identities. So, this should error.
+			testUtils.UpdateDoc{
+				Identity:      testUtils.NoIdentity(),
+				CollectionID:  0,
+				DocID:         0,
+				Doc:           `{ "name": "Lone" }`,
+				ExpectedError: "not authorized to perform operation",
+			},
+			testUtils.Request{ // Should not be updated
+				Identity: testUtils.ClientIdentity(1),
+				Request:  `query{ User { name } }`,
+				Results: map[string]any{
+					"User": []map[string]any{{"name": "Shahzad"}},
+				},
+			},
+
+			// Wrong user/identity will also not be authorized.
+			testUtils.UpdateDoc{
+				Identity:      testUtils.ClientIdentity(2),
+				CollectionID:  0,
+				DocID:         0,
+				Doc:           `{ "name": "Lone" }`,
+				ExpectedError: "not authorized to perform operation",
+			},
+			testUtils.Request{ // Should not be updated
+				Identity: testUtils.ClientIdentity(1),
+				Request:  `query{ User { name } }`,
+				Results: map[string]any{
+					"User": []map[string]any{{"name": "Shahzad"}},
+				},
+			},
+
+			// This should work as the identity is authorized.
+			testUtils.UpdateDoc{
+				Identity:     testUtils.ClientIdentity(1),
+				CollectionID: 0,
+				DocID:        0,
+				Doc:          `{ "name": "Lone" }`,
+			},
+			testUtils.Request{ // Should now be updated
+				Identity: testUtils.ClientIdentity(1),
+				Request:  `query{ User { name } }`,
+				Results: map[string]any{
+					"User": []map[string]any{{"name": "Lone"}},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
+
+func TestNAC_GatesDocUpdateWithFilter_AllowIfAuthorizedElseError(t *testing.T) {
+	test := testUtils.TestCase{
+		Description: "admin acp correctly gates doc update with filter operation, allow if authorized, otherwise error",
+		Actions: []any{
+			// Starting with NAC, so only authorized user(s) can perform operations from here on out.
+			testUtils.Close{},
+			testUtils.Start{
+				Identity:  testUtils.ClientIdentity(1),
+				EnableNAC: true,
+			},
+			// Note: Doing setup steps after starting with nac enabled, otherwise the in-memory tests
+			// will loose setup state when the restart happens (i.e. the restart that started nac).
+			testUtils.SchemaUpdate{
+				Identity: testUtils.ClientIdentity(1),
+				Schema: `
+					type User {
+						name: String
+						age: Int 
+					}`,
+			},
+			testUtils.CreateDoc{
+				Identity:     testUtils.ClientIdentity(1),
+				CollectionID: 0,
+				Doc: `{
+					"name": "Shahzad",
+					"age": 48
+				}`,
+			},
+
+			// We haven't authorized non-identities. So, this should error.
+			testUtils.UpdateWithFilter{
+				Identity:      testUtils.NoIdentity(),
+				CollectionID:  0,
+				Filter:        `{name: {_eq: "Shahzad"}}`,
+				Updater:       `{"name": "Lone"}`,
+				ExpectedError: "not authorized to perform operation",
+			},
+			testUtils.Request{ // Should not be updated
+				Identity: testUtils.ClientIdentity(1),
+				Request:  `query{ User { name } }`,
+				Results: map[string]any{
+					"User": []map[string]any{{"name": "Shahzad"}},
+				},
+			},
+
+			// Wrong user/identity will also not be authorized.
+			testUtils.UpdateWithFilter{
+				Identity:      testUtils.ClientIdentity(2),
+				CollectionID:  0,
+				Filter:        `{name: {_eq: "Shahzad"}}`,
+				Updater:       `{"name": "Lone"}`,
+				ExpectedError: "not authorized to perform operation",
+			},
+			testUtils.Request{ // Should not be updated
+				Identity: testUtils.ClientIdentity(1),
+				Request:  `query{ User { name } }`,
+				Results: map[string]any{
+					"User": []map[string]any{{"name": "Shahzad"}},
+				},
+			},
+
+			// This should work as the identity is authorized.
+			testUtils.UpdateWithFilter{
+				Identity:     testUtils.ClientIdentity(1),
+				CollectionID: 0,
+				Filter:       `{name: {_eq: "Shahzad"}}`,
+				Updater:      `{"name": "Lone"}`,
+			},
+			testUtils.Request{ // Should now be updated
+				Identity: testUtils.ClientIdentity(1),
+				Request:  `query{ User { name } }`,
+				Results: map[string]any{
+					"User": []map[string]any{{"name": "Lone"}},
+				},
+			},
+		},
+	}
+
+	testUtils.ExecuteTestCase(t, test)
+}
